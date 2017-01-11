467ba9bdec28ba44119668713e02ae6160a44e9b
Taniraw <tanirawrites@gmail.com>
2017-01-11 17:54:13 -0500

    useLoginFlow

---
+++ BrooklynCommunityBoards/AppController.swift
@@ -0,0 +1,9 @@
+//
+//  AppController.swift
+//  BrooklynCommunityBoards
+//
+//  Created by Tanira Wiggins on 1/11/17.
+//  Copyright © 2017 Tanira Wiggins. All rights reserved.
+//
+
+import Foundation
