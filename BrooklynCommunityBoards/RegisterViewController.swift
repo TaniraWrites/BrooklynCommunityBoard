ce3a837033e5a08587af6034b195d6cb74639f89
Taniraw <tanirawrites@gmail.com>
2017-01-23 14:35:16 -0500

    storyboardSetup

---
+++ BrooklynCommunityBoards/RegisterViewController.swift
@@ -0,0 +1,34 @@
+//
+//  RegisterViewController.swift
+//  BrooklynCommunityBoards
+//
+//  Created by Tanira Wiggins on 1/23/17.
+//  Copyright © 2017 Tanira Wiggins. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var streetTextField: UITextField!
+    @IBOutlet weak var zipCodeTextField: UITextField!
+    @IBOutlet weak var cityStateTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+  
+    
+
+
+}
